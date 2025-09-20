050d199f806f3f55d0a4ac5168213197b44c1b5a
2302234-sys <2302234@ub.edu.ph>
2025-09-20 15:04:34 +0800

    EXAMPLE 1&2

--- student_mis/src/main.jsx
+++ student_mis/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+
+//import { createRoot } from 'react-dom/client'
+
+//const myElement = <h1>EXAMPLE 1: I Love JSX!</h1>;
+
+//createRoot(document.getElementById('root')).render(
+  //myElement
+//);
+
+import React from 'react';
+import { createRoot, createElement } from 'react-dom/client'
+
+const myElement = React.createElement('h1', {}, ' Example 2: I do not use JSX!');
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  myElement
+);
