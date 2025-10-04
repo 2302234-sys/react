ad51310d900069b725a3436c9bdd5aaf70be691f
Comia <2302234@ub.edu.ph>
2025-10-04 14:59:14 +0800

    Expression

--- student_mis/src/example/expression.jsx
+++ student_mis/src/example/expression.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 
 export function Expression() {
   const hp = 218 * 1.36;
-
+  const myobj = {
+    name: "Fiat",
+    model: "500",
+    color: "white"
+ 
+}
   return (
     <>
       <h1>Expression Examples 1</h1>
@@ -12,6 +17,9 @@ export function Expression() {
       <h1>Expression Examples 2</h1>
       <h2>My car</h2>
       <p>It has {hp} horsepower</p> 
+
+      <h1>Expression Examples 3</h1>
+      <h1>My car is a {myobj.color} {myobj.name} {myobj.model}</h1>
     </>
   );
 }
